edb793795d56439ebb092dee40b417bfdee62154
Mohammed Alhaddar <moalhaddar@gmail.com>
2020-01-06 18:45:49 +0300

    Solved

--- functions.js
+++ functions.js
@@ -7,6 +7,7 @@
  ****************************************************************/
 function getBookById(bookId, books) {
   // Your code goes here
+  return books.find( (book) => book.id == bookId)
 }
 
 /**************************************************************
@@ -18,6 +19,7 @@ function getBookById(bookId, books) {
  ****************************************************************/
 function getAuthorByName(authorName, authors) {
   // Your code goes here
+  return authors.find( author => author.name.toLowerCase() == authorName.toLowerCase())
 }
 
 /**************************************************************
@@ -27,7 +29,7 @@ function getAuthorByName(authorName, authors) {
  *    [{ author: <NAME>, bookCount: <NUMBER_OF_BOOKS> }]
  ****************************************************************/
 function bookCountsByAuthor(authors) {
-  // Your code goes here
+  return authors.map( (auth) => ({ author: auth.name, bookCount: auth.books.length}))
 }
 
 /**************************************************************
@@ -41,7 +43,13 @@ function booksByColor(books) {
   const colors = {};
 
   // Your code goes here
-
+  books.forEach( book => {
+    if (!colors[book.color]) {
+      colors[book.color] = []
+    }
+    colors[book.color].push(book.title)
+    
+  })
   return colors;
 }
 
@@ -54,7 +62,11 @@ function booksByColor(books) {
  *    ["The Hitchhikers Guide", "The Meaning of Liff"]
  ****************************************************************/
 function titlesByAuthorName(authorName, authors, books) {
-  // Your code goes here
+  return books.filter( book => {
+    return book.authors.filter( auth => {
+      return auth.name.toLowerCase() == authorName.toLowerCase();
+    }).length != 0
+  }).map( (book) => (book.title))
 }
 
 /**************************************************************
@@ -65,7 +77,13 @@ function titlesByAuthorName(authorName, authors, books) {
  * Note: assume there will never be a tie
  ****************************************************************/
 function mostProlificAuthor(authors) {
-  // Your code goes here
+  return authors.reduce( (prev, current) => {
+    if (current.books.length > prev.books.length ){
+      return current
+    }else{
+      return prev
+    }
+  }).name
 }
 
 /**************************************************************
@@ -92,7 +110,14 @@ function mostProlificAuthor(authors) {
  * BONUS: REMOVE DUPLICATE BOOKS
  ****************************************************************/
 function relatedBooks(bookId, authors, books) {
-  // Your code goes here
+
+  const targetAuths = books.find( (book) => book.id == bookId ).authors.map( auth => auth.name)
+
+  return books.filter( book => {
+    const intersection = book.authors.map( auth => auth.name ).filter(element => targetAuths.includes(element));
+    return intersection.length != 0
+  }).map( (book) => (book.title))
+
 }
 
 /**************************************************************
@@ -103,6 +128,13 @@ function relatedBooks(bookId, authors, books) {
  ****************************************************************/
 function friendliestAuthor(authors) {
   // Your code goes here
+  return authors.reduce( (prev, current) => {
+    if (current.books.length > prev.books.length ){
+      return current
+    }else{
+      return prev
+    }
+  }).name
 }
 
 module.exports = {
@@ -121,8 +153,8 @@ module.exports = {
  * want to manually test your code
  */
 
-// const authors = require("./authors.json");
-// const books = require("./books.json");
+const authors = require("./authors.json");
+const books = require("./books.json");
 
 // console.log(getBookById(12, books));
 // console.log(getAuthorByName("J.K. Rowling", authors));
@@ -130,5 +162,5 @@ module.exports = {
 // console.log(booksByColor(books));
 // console.log(titlesByAuthorName("George R.R. Martin", authors, books));
 // console.log(mostProlificAuthor(authors));
-// console.log(relatedBooks(50, authors, books));
+console.log(relatedBooks(46, authors, books));
 // console.log(friendliestAuthor(authors));
